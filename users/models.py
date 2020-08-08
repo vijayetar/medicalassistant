41a70fe296a2b2c7d07ef9ccbd78ee3638049579
Vijayeta Rangarajan <vijayetar@gmail.com>
2020-08-07 22:57:48 -0700

    users working

--- users/models.py
+++ users/models.py
@@ -1,3 +1,18 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+
+
+class CustomUser(AbstractUser):
+	email = models.EmailField(verbose_name = 'email', max_length = 160, unique=True)
+	username = models.CharField(max_length=30, unique = True)
+	date_joined = models.DateTimeField(verbose_name = 'date joined', auto_now_add = True)
+	first_name = models.CharField(max_length = 64)
+	last_name = models.CharField(max_length = 64)
+	
+	USERNAME_FIELD = 'email'
+	REQUIRED_FIELDS = ['username']
+	
+	def __str__(self):
+		return self.username
